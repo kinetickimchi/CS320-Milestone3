2c8134176e90987ffbddfef81457af82affd0500
kinetickimchi <111025844+kinetickimchi@users.noreply.github.com>
2024-09-28 19:49:50 -0400

    fix ContactService function signatures

--- src/main/java/ContactService.java
+++ src/main/java/ContactService.java
@@ -31,25 +31,48 @@ public class ContactService {
         return list.removeIf(contact -> contact.getId().equalsIgnoreCase(id));
     }
 
-    // updateContact function for firstName, lastName, phoneNumber, address
-    public boolean updateContact(String id, String firstName, String lastName, String phoneNumber, String address) {
+    // Separate functions to update fields, to only take in the required information
+    public boolean updateContactFirstName(String id, String firstName) {
         for (Contact contact : list) {
             if (contact.getId().equalsIgnoreCase(id)) {
-                if (firstName != null) {
+                if (firstName != null && firstName.length() <= 10) {
                     contact.setFirstName(firstName);
                 }
-                if (lastName != null) {
-                    contact.setLastName(lastName);
-                }
-                if (phoneNumber != null) {
-                    contact.setPhoneNumber(phoneNumber);
-                }
-                if (address != null) {
-                    contact.setAddress(address);
-                }
-                return true;
             }
         }
-        return false;  // if ID match is not found
+        return true; // if id match is not found
+    }
+
+    public boolean updateContactLastName(String id, String lastName) {
+        for (Contact contact : list) {
+            if (contact.getId().equalsIgnoreCase(id)) {
+                if (lastName != null && lastName.length() <= 10) {
+                    contact.setLastName(lastName);
+                }
+            }
+        }
+        return true; // if id match is not found
+    }
+
+    public boolean updateContactPhoneNumber(String id, String phoneNumber) {
+        for (Contact contact : list) {
+            if (contact.getId().equalsIgnoreCase(id)) {
+                if (phoneNumber != null && phoneNumber.length() == 10 && phoneNumber.matches("\\d+")) {
+                    contact.setPhoneNumber(phoneNumber);
+                }
+            }
+        }
+        return true; // if id match is not found
+    }
+
+    public boolean updateContactAddress(String id, String address) {
+        for (Contact contact : list) {
+            if (contact.getId().equalsIgnoreCase(id)) {
+                if (address != null && address.length() <= 30) {
+                    contact.setAddress(address);
+                }
+            }
+        }
+        return true; // if id match is not found
     }
 }
